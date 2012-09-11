a8a402ce7bd6551f396fa992f8625611cb05fc6d
Janne Uitto <zigilii@msn.com>
2012-09-11 18:41:13 +0300

    keskittää kun pallo loittonee
    pari funktioo listätty, yritetty selkeyttää koodia

--- lib/pingpong_client.rb
+++ lib/pingpong_client.rb
@@ -1,7 +1,6 @@
 require 'socket'
 require 'rubygems'
 require 'json'
-require 'fileutils'
 require 'launchy'
 
 
@@ -9,7 +8,7 @@ module Pingpong
   class Client
     @@messagecounter = [0,0,0,0,0,0,0,0,0,0,0]
 	@@speed = 0
-	
+	@@prevBallX = 0;
 	
 	def initialize(player_name, server_host, server_port)
       tcp = TCPSocket.open(server_host, server_port)
@@ -35,35 +34,30 @@ module Pingpong
           when 'gameIsOn'
             #puts "\nChallenge from server: #{json}\n"
 			#puts @@speed
-            if (message['data']['ball']['pos']['y']) < (message['data']['conf']['paddleHeight'] / 2 + message['data']['left']['y'])
-				if(@@speed != -1.0)
-					if(countmessages(message['data']['time'])) 
-						puts "Up"
-						tcp.puts movement_message(-1.0)
-						@@speed = -1.0
-					end
-				end
-			elsif (message['data']['ball']['pos']['y'])  > (message['data']['conf']['paddleHeight'] / 2 + message['data']['left']['y'])
-				if(@@speed != 1.0)
-					if(countmessages(message['data']['time'])) 
-						puts "Down"
-						tcp.puts movement_message(1.0)
-						@@speed = 1.0
-					end
+			moveThreshold = 10
+			if(directionIsLeft(message['data']['ball']['pos']['x']))
+				## direction to you
+				if (message['data']['ball']['pos']['y']) < (message['data']['conf']['paddleHeight'] / 2 + message['data']['left']['y'])
+					moveUp(message, tcp)
+				elsif (message['data']['ball']['pos']['y']) > (message['data']['conf']['paddleHeight'] / 2 + message['data']['left']['y'])
+					moveDown(message, tcp)
+				else
+					moveStay(message, tcp)
 				end
 			else
-				if(@@speed != 0.0)
-					if(countmessages(message['data']['time'])) 
-						puts "Stay"
-						tcp.puts movement_message(0)
-						@@speed = 0
-					end
+				## direction away from me
+				if ((message['data']['conf']['paddleHeight'] / 2 + message['data']['left']['y']) > (message['data']['conf']['maxHeight'] / 2 + moveThreshold))
+					moveUp(message, tcp)
+				elsif ((message['data']['conf']['paddleHeight'] / 2 + message['data']['left']['y']) < (message['data']['conf']['maxHeight'] / 2 - moveThreshold))
+					moveDown(message, tcp)
+				else
+					moveStay(message, tcp)
 				end
 			end
 		else
 			puts "Undefined message"
-			@@speed = 0.001
-			react_to_messages_from_server tcp
+			@@speed = 0.01
+			#react_to_messages_from_server tcp
         end
       end
     end
@@ -77,28 +71,64 @@ module Pingpong
     end
 	
 	def countmessages(timestamp)
-		@@messagecounter[10] = timestamp
-		if @@messagecounter[0] == 0
-			for i in 1..10 #t‰ss‰ siirret‰‰n kaikkia taulukon arvoja yhdell‰ vasemmalle
-				@@messagecounter[i-1] = @@messagecounter[i]
-				puts @@messagecounter[i-1]
+		@@messagecounter[10] = timestamp	# tallennetaan uusin aika taulukkoon jotta voidaan tarkistaa ett‰ ollaanko liikaa k‰skytetty
+		if @@messagecounter[0] == 0 || @@messagecounter[10] > @@messagecounter[0] + 1000	# jos ei olla viel‰ k‰skytetty 10 kertaa ylip‰‰t‰‰n
+			for i in 1..10 																	# tai 10. k‰skytus on yli sekunnin p‰‰ss‰
+				@@messagecounter[i-1] = @@messagecounter[i] #t‰ss‰ siirret‰‰n kaikkia taulukon arvoja yhdell‰ vasemmalle
+				#puts @@messagecounter[i-1]
 			end
-			puts "Count messages true < 10"
-			return true
-		elsif @@messagecounter[10] > @@messagecounter[0] + 1000
-			for i in 1..10 #t‰ss‰ siirret‰‰n kaikkia taulukon arvoja yhdell‰ vasemmalle
-				@@messagecounter[i-1] = @@messagecounter[i]
-				puts @@messagecounter[i-1]
-			end
-			puts "Count messages true"
+			#puts "Count messages TRUE"
 			return true
 		else
 			puts "Count messages FALSE"
 			return false
 		end
 	end	
+	
+	def directionIsLeft(currentX)
+		if( currentX < @@prevBallX)
+			@@prevBallX = currentX
+			return true
+		else
+			@@prevBallX = currentX
+			return false
+		end
+	end
+	
+	def moveUp(message, tcp)
+		if(@@speed != -1.0) ## jos menn‰‰n jo ylˆs t‰ytt‰ vauhtia niin ei muuteta mit‰‰n
+			if(countmessages(message['data']['time']))  # tarkistetaan 10 komentoa/sec
+				puts "Up"
+				tcp.puts movement_message(-1.0)
+				@@speed = -1.0
+			end
+		end
+	end
+	
+	def moveDown(message, tcp)
+		if(@@speed != 1.0)
+			if(countmessages(message['data']['time'])) 
+				puts "Down"
+				tcp.puts movement_message(1.0)
+				@@speed = 1.0
+			end
+		end
+	end
+	
+	def moveStay(message, tcp)
+		if(@@speed != 0.0)
+			if(countmessages(message['data']['time'])) 
+				puts "Stay"
+				tcp.puts movement_message(0)
+				@@speed = 0
+			end
+		end
+	end
+	
+	
   end
 end
+
 
 player_name = ARGV[0]
 server_host = ARGV[1]
